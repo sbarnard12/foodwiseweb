2df22b8e478e7e38a797a4a8f229681403ae08dc
Sam Barnard <sambarnard12@gmail.com>
2017-03-23 12:53:36 -0400

    t

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,9 +13,16 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
+ app.set('view engine', 'hbs');
+ app.use (
+   sassMiddleware({
+     src: __dirname + '/sass',
+     dest: __dirname + '/public',
+     // prefix: '/stylesheets',
+     debug: true,
+   })
+ );
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/fonts', express.static(path.join(__dirname, 'node_modules/bootstrap-sass/assets/fonts')));
 
 app.use('/', index);
 app.use('/users', users);
